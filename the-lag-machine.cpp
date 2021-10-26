6f5a93e1f11a2242836b23b473cbe77b3648e3c7
the programmer <88006978+mitodocpp@users.noreply.github.com>
2021-10-26 15:53:53 -0300

    Create the-lag-machine.cpp

---
+++ the-lag-machine.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+while(1 == 1)
+{
+cout << "iashfioshuifcnfcyg fygh f837g87gv86gt8aeguyg8tg87eg87eagf 8e7ghf8esnv7egh r gdv g48674g68v v 99i8r09  r ";
+cout << "fdshjjknfd4iejfwdr  r898uf8uwer89g8 8h e5te tv 41tr534550,530,01v ttfg fg fg fg r41en  ncjufrncjskdzfnvif ffdrs fdrv";
+}
+}
